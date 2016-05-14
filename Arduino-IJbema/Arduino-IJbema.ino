e5a58799d8f7d85835d76aed2e56a70eba9affd6
Thomas ten Cate <ttencate@gmail.com>
2016-05-14 22:01:19 +0200

    Laat quotes scrollen

--- Arduino-IJbema/Arduino-IJbema.ino
+++ Arduino-IJbema/Arduino-IJbema.ino
@@ -3,6 +3,7 @@
 #include <LiquidCrystal.h>
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+int const LCD_WIDTH = 16;
 
 void setup() {
   // Read on unconnected pin to get a somewhat random seed.
@@ -16,12 +17,29 @@ void setup() {
 void showRandomQuote() {
   lcd.clear();
   
-  lcd.setCursor(0, 0);
+  lcd.home();
   lcd.print("Mark zou zeggen:");
   
-  lcd.setCursor(0, 1);
-  int quoteIndex = random(NUM_QUOTES);
-  lcd.print(QUOTES[quoteIndex]);  
+  char const *quote = QUOTES[random(NUM_QUOTES)];
+  int quoteLength = strlen(quote);
+  for (int scrollOffset = -16; scrollOffset <= quoteLength; scrollOffset++) {
+    char buffer[LCD_WIDTH + 1];
+    for (int i = 0; i < LCD_WIDTH; i++) {
+      int j = i + scrollOffset;
+      if (j >= 0 && j < quoteLength) {
+        buffer[i] = quote[j];
+      } else {
+        buffer[i] = ' ';
+      }
+    }
+    buffer[LCD_WIDTH] = '\0';
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+    delay(200);
+  }
+
+  delay(500);
+  lcd.clear();
 }
 
 void loop() {
