e58dd697d684dd7e6b15f76e7e7870ba1489da4e
Thomas ten Cate <ttencate@gmail.com>
2016-05-14 21:45:44 +0200

    Show a random quote (out of 3)

---
+++ Arduino-IJbema/quotes.cpp
@@ -0,0 +1,9 @@
+#include "quotes.h"
+
+char const *const QUOTES[] = {
+  "Eerst de afwas",
+  "De stekker is eruit!",
+  "We waren juist op zoek naar geile jongens",
+};
+
+int const NUM_QUOTES = sizeof(QUOTES) / sizeof(char const *);
---
+++ Arduino-IJbema/quotes.h
@@ -0,0 +1,7 @@
+#ifndef QUOTES_H_
+#define QUOTES_H_
+
+extern char const *const QUOTES[];
+extern int const NUM_QUOTES;
+
+#endif
