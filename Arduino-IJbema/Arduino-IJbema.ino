7a094e5536ed7d29b47d0c39753b1bed7a18694e
Thomas ten Cate <ttencate@gmail.com>
2016-05-15 13:08:14 +0200

    Read quotes from flash memory

--- Arduino-IJbema/Arduino-IJbema.ino
+++ Arduino-IJbema/Arduino-IJbema.ino
@@ -1,9 +1,75 @@
 #include "quotes.h"
 
+#include <avr/pgmspace.h>
 #include <LiquidCrystal.h>
+
+static char const SUBJECT_PREFIX[] PROGMEM = "Mark over ";
+static char const SUBJECT_SUFFIX[] PROGMEM = ":";
+
+namespace {
 
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 int const LCD_WIDTH = 16;
+
+void fillWithSpaces(char *buffer, int length) {
+  buffer[length] = '\0';
+  for (; length >= 0; length--) {
+    buffer[length] = ' ';
+  }
+}
+
+void shiftLeft(char *buffer, int length) {
+  for (int i = 0; i < length; i++) {
+    buffer[i] = buffer[i + 1];
+  }
+}
+
+void showRandomQuote() {
+  
+  int quoteIndex = random(NUM_QUOTES);
+  char const *subject = (char const *) pgm_read_word_near(SUBJECTS + quoteIndex);
+  char const *quote = (char const *) pgm_read_word_near(QUOTES + quoteIndex);
+  
+  char buffer[LCD_WIDTH + 1];
+  char *end = buffer + LCD_WIDTH;
+  char *write = buffer;
+  write += strlcpy_P(buffer, SUBJECT_PREFIX, LCD_WIDTH);
+  if (end - write > 0) {
+    write += strlcpy_P(write, subject, end - write);
+  }
+  if (end - write > 0) {
+    write += strlcpy_P(write, SUBJECT_SUFFIX, LCD_WIDTH);
+  }
+  
+  lcd.clear();
+  lcd.home();
+  lcd.print(buffer);
+  
+  fillWithSpaces(buffer, LCD_WIDTH);
+  while (char c = pgm_read_byte_near(quote)) {
+    shiftLeft(buffer, LCD_WIDTH);
+    buffer[LCD_WIDTH - 1] = c;
+    
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+    delay(200);
+
+    quote++;
+  }
+  for (int i = 0; i < LCD_WIDTH; i++) {
+    shiftLeft(buffer, LCD_WIDTH);
+    buffer[LCD_WIDTH - 1] = ' ';
+    
+    lcd.setCursor(0, 1);
+    lcd.print(buffer);
+    delay(200);
+  }
+
+  delay(500);
+  lcd.clear();
+}
+
+} // namespace
 
 void setup() {
   // Read on unconnected pin to get a somewhat random seed.
@@ -12,39 +78,6 @@ void setup() {
   pinMode(13, INPUT);
   
   lcd.begin(16, 2);
-}
-
-void showRandomQuote() {
-  lcd.clear();
-  
-  lcd.home();
-  lcd.print("Mark zou zeggen:");
-
-  int quoteIndex = random(NUM_QUOTES);
-  Serial.print("Selected quote ");
-  Serial.print(quoteIndex);
-  Serial.print(" out of ");
-  Serial.print(NUM_QUOTES);
-  char const *quote = QUOTES[quoteIndex][0];
-  int quoteLength = strlen(quote);
-  for (int scrollOffset = -16; scrollOffset <= quoteLength; scrollOffset++) {
-    char buffer[LCD_WIDTH + 1];
-    for (int i = 0; i < LCD_WIDTH; i++) {
-      int j = i + scrollOffset;
-      if (j >= 0 && j < quoteLength) {
-        buffer[i] = quote[j];
-      } else {
-        buffer[i] = ' ';
-      }
-    }
-    buffer[LCD_WIDTH] = '\0';
-    lcd.setCursor(0, 1);
-    lcd.print(buffer);
-    delay(200);
-  }
-
-  delay(500);
-  lcd.clear();
 }
 
 void loop() {
--- Arduino-IJbema/quotes.h
+++ Arduino-IJbema/quotes.h
@@ -1,7 +1,8 @@
 #ifndef QUOTES_H_
 #define QUOTES_H_
 
-extern char const *const QUOTES[][2];
+extern char const *const SUBJECTS[];
+extern char const *const QUOTES[];
 
 extern int const NUM_QUOTES;
 
