cd17f6a4aa39eab4e91dcd1081669179666df4fb
Erik Weitenberg <e.r.a.weitenberg@gmail.com>
2016-05-15 12:58:04 +0200

    Add quotes to source
    Note that currently the program is too large to fit on a Uno. It seems we are keeping the data both in the 'sketch' and the 'dynamic memory'.

--- Arduino-IJbema/quotes.cpp
+++ Arduino-IJbema/quotes.cpp
@@ -1,9 +1,7 @@
 #include "quotes.h"
 
-char const *const QUOTES[] = {
-  "Eerst de afwas",
-  "De stekker is eruit!",
-  "We waren juist op zoek naar geile jongens",
+static char const *const QUOTES[] = {
+#include "quotes.txt" /* If using Arudino on winodws, you'll need to enter full path */
 };
 
 int const NUM_QUOTES = sizeof(QUOTES) / sizeof(char const *);
