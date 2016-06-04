0b836ff67b0dbc125916e8777a144cfd2a2418e8
Thomas ten Cate <ttencate@gmail.com>
2016-06-04 14:05:20 +0200

    Move tetris.ino code into the main program

--- Arduino-IJbema/Arduino-IJbema.ino
+++ Arduino-IJbema/Arduino-IJbema.ino
@@ -1,5 +1,6 @@
-#include "quotes.h"
 #include "decompress.h"
+#include "quotes.h"
+#include "tetris.h"
 
 #include <avr/pgmspace.h>
 #include <LiquidCrystal.h>
@@ -63,12 +64,26 @@ void showRandomQuote() {
   lcd.clear();
 }
 
+void playTetris() {
+  Tetris tetris(15, 10, lcd);
+  tetris.mapButton(6, TetrisButton::MOVE_LEFT);
+  tetris.mapButton(7, TetrisButton::SOFT_DROP);
+  tetris.mapButton(8, TetrisButton::ROTATE_RIGHT);
+  tetris.mapButton(9, TetrisButton::MOVE_RIGHT);
+  tetris.play();
+}
+
 } // namespace
 
 void setup() {
+  pinMode(0, INPUT);
   // Read on unconnected pin to get a somewhat random seed.
   randomSeed(analogRead(0));
-  
+
+  pinMode(6, INPUT);
+  pinMode(7, INPUT);
+  pinMode(8, INPUT);
+  pinMode(9, INPUT);
   pinMode(13, INPUT);
   
   lcd.begin(16, 2);
